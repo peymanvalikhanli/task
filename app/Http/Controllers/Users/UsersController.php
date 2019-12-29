e83b5fdc5d205e06b3d1ac20c873308950288f37
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2019-12-29 11:18:30 -0800

    create model file and controller file

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -5,7 +5,12 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\UsersModel; 
+
 class UsersController extends Controller
 {
     //
+    public function user(){
+        return return response()->json(UsersModelRoute::get(), 200);
+    }
 }
